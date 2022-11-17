9a94359b9eba70772c53f1642ceac0c3c0ce1819
ZhenyaIlchenko <108277664+ZhenyaIlchenko@users.noreply.github.com>
2022-11-17 14:28:24 +0200

    1 task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+const galleryItemEl = galleryItems
+  .map(
+    item =>
+      `<div class="gallery__item">
+<a class="gallery__item" href="${item.original}">
+<img
+      class="gallery__image"
+      src="${item.preview}"
+      alt="${item.description}"/></a></div>`
+  )
+  .join('');
+galleryEl.insertAdjacentHTML('beforeend', galleryItemEl);
+// console.log(galleryEl);
+
+const gallery = new SimpleLightbox('.gallery a', {
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
