2433900cfeb01cddcc28147bc6b38679d467d578
ZhenyaIlchenko <108277664+ZhenyaIlchenko@users.noreply.github.com>
2022-11-18 01:01:37 +0200

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+};
+
+let formData = {};
+
+function populateData() {
+  const saveData = localStorage.getItem(STORAGE_KEY);
+
+  if (saveData) {
+    const parsedData = JSON.parse(saveData);
+
+    for (const key in parsedData) {
+      if (Object.hasOwnProperty.call(parsedData, key)) {
+        refs.form[key].value = parsedData[key];
+      }
+    }
+    formData = parsedData;
+  }
+}
+populateData();
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onInput, 500));
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  console.log('Form is submitted');
+  event.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function onInput(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
