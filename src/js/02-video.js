ba85ffd4e84c5c5849fb2fee2443367b1377a534
ZhenyaIlchenko <108277664+ZhenyaIlchenko@users.noreply.github.com>
2022-11-18 15:50:44 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+function onTimeUpdate(event) {
+  const time = event.seconds;
+  localStorage.setItem(STORAGE_KEY, time);
+}
+
+function setCurrentTime() {
+  const currentTime = localStorage.getItem(STORAGE_KEY);
+  if (currentTime) {
+    player.setCurrentTime(currentTime);
+  }
+}
+setCurrentTime();
